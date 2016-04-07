c369e347aad5d92f040a1d51336d8798046dd9e9
carolx <carol.xu@ehealth-china.com>
2016-04-07 09:31:05 +0800

    maven project to test jenkins

---
+++ src/main/java/com/ehealth/helloworld/helloworld/App.java
@@ -0,0 +1,17 @@
+package com.ehealth.helloworld.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public String sayHello(){
+		return "Hello World!";
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( new App().sayHello() );
+    }
+}
